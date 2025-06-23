43cdc39a356708a3544711ed5b5333f0d6ef5642
Tranhuynhvu <tranhuynhvu2k4@gmail.com>
2025-06-23 14:15:26 +0700

    Create  ProfileController

---
+++ BackEnd_Api/Helpers/JwtTokenHelper.cs
@@ -0,0 +1,36 @@
+﻿using BackEnd_Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BackEnd_Api.Helpers
+{
+    public class JwtTokenHelper
+    {
+        private readonly IConfiguration _config;
+        public JwtTokenHelper( IConfiguration config)
+        {
+            _config = config;
+        }
+        private string GenerateJwtToken(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id)
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.UtcNow.AddHours(3),
+                claims: claims,
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
