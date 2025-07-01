a8937c5d58a60365c78927e0c886a7bf2055a93c
thaithanhdat011 <thaithanhdatqn2019@gmail.com>
2025-07-01 23:10:36 +0700

    screen evidence and screen investigation

--- FrontEnd_App/src/App.jsx
+++ FrontEnd_App/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Home from "./pages/home";
 import ProfilePage from "./pages/profile";
+import Investigation from "./pages/investigation";
+import Evidence from "./pages/evidence";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
          <Route path="/profile" element={<ProfilePage />} />
+         <Route path="/investigation" element={<Investigation />} />
+         <Route path="/evidence" element={<Evidence />} />
       </Routes>
     </Router>
   );
