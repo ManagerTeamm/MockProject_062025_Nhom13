69c0aaae27bb8d1f337bea47813e849d1410778e
txqt <cv.hovanthanh@gmail.com>
2025-07-01 20:21:42 +0700

    home page view

--- FrontEnd_App/src/App.jsx
+++ FrontEnd_App/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Home from "./pages/home";
 import ProfilePage from "./pages/profile";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<LoginPage />} />
          <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
