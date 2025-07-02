80205d7fbe073ba56e040e9a9e83d79d603c6cca
Khanh Nguyen Tran Nha <nhakhanhntn@gmail.com>
2025-07-02 22:43:45 +0700

    Creat Component RelevantParties

--- FrontEnd_App/src/App.jsx
+++ FrontEnd_App/src/App.jsx
@@ -7,6 +7,7 @@ import Evidence from "./pages/evidence";
 import UserList from "./pages/admin/userList";
 import ReportPage from "./pages/report";
 
+
 function App() {
   return (
     <Router>
@@ -17,6 +18,7 @@ function App() {
         <Route path="/evidence" element={<Evidence />} />
         <Route path="/admin/userlist" element={<UserList /> } />
         <Route path="/report-manager" element={<ReportPage />} />
+     
       </Routes>
     </Router>
   );
